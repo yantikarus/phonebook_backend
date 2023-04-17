f517db1c3e67b971f6d7b060557c394a7bca2fe1
Yanti <wsutr@hotmail.com>
2023-04-16 21:37:50 -0600

    add mongo.js to create data in database based on cmd arguments

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+  }
+
+const password = process.argv[2]
+const contactName = process.argv[3]
+const contactNumber = process.argv[4]
+
+const url = `mongodb+srv://wsutr:${password}@cluster0.vu3tk4f.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const phoneSchema = new mongoose.Schema({
+    name:String,
+    number:Number,
+})
+
+const Person = mongoose.model('Person', phoneSchema)
+
+const person = new Person({
+    name:contactName,
+    number:contactNumber,
+})
+if (process.argv.length == 3){
+        console.log("only 3 argument")
+        Person.find({}).then(result =>{
+            result.forEach (person =>{
+                console.log(person)
+            })
+            mongoose.connection.close()
+        })
+      }
+else{
+    person.save().then(result =>{
+        console.log("person saved!")
+        mongoose.connection.close()
+    })
+}
+
+
+

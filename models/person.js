4a279041fe4ae7a5fa98618a5e518d836328c1cc
Yanti <wsutr@hotmail.com>
2023-04-19 15:45:45 -0600

    added eslint and fix errors

--- models/person.js
+++ models/person.js
@@ -3,39 +3,37 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-console.log("connecting to ", url)
+console.log('connecting to ', url)
 
 mongoose.connect(url)
-.then(result => {
-    console.log("connected to phonebook database")
-})
-.catch((error)=>{
-    console.log("error connecting to MongoDB", error.message)
-})
+  .then(console.log('connected to phonebook database'))
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 const phoneSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        minLength:3,
-        required:true
+  name: {
+    type:String,
+    minLength:3,
+    required:true
+  },
+  number:{
+    type:String,
+    minLength:8,
+    validate: {
+      validator: function(v){
+        return /^[0-9]{2,3}-[0-9]*$/.test(v)
+      }
     },
-    number:{
-        type:String,
-        minLength:8,
-        validate: {
-            validator: function(v){
-                return /^[0-9]{2,3}-[0-9]*$/.test(v);
-            }
-        },
-        required:true
-    }
+    required:true
+  }
 })
 phoneSchema.set('toJSON', {
-    transform: (document, returnObject)=>{
-        returnObject.id = returnObject._id.toString()
-        delete returnObject._id
-        delete returnObject.__v
-    }
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString()
+    delete returnObject._id
+    delete returnObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', phoneSchema)
--- mongo.js
+++ mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-  }
+  console.log('give password as argument')
+  process.exit(1)
+}
 
 const password = process.argv[2]
 const contactName = process.argv[3]
@@ -15,31 +15,31 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const phoneSchema = new mongoose.Schema({
-    name:String,
-    number:Number,
+  name:String,
+  number:Number,
 })
 
 const Person = mongoose.model('Person', phoneSchema)
 
 const person = new Person({
-    name:contactName,
-    number:contactNumber,
+  name:contactName,
+  number:contactNumber,
 })
-if (process.argv.length == 3){
-        console.log("only 3 argument")
-        Person.find({}).then(result =>{
-            result.forEach (person =>{
-                console.log(person)
-            })
-            mongoose.connection.close()
-        })
-      }
-else{
-    person.save().then(result =>{
-        console.log("person saved!")
-        mongoose.connection.close()
+if (process.argv.length === 3){
+  console.log('only 3 argument')
+  Person.find({}).then(result => {
+    result.forEach (person => {
+      console.log(person)
     })
+    mongoose.connection.close()
+  })
 }
+else{
+// eslint-disable-next-line no-unused-vars
+  person.save().then(x => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })}
 
 
 
